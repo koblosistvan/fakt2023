a67a02e58efa80cd64eda4e310de850b986ccffc
uranusz26 <milan.lohner10@gmail.com>
2023-09-07 12:21:50 +0200

    kezdés

--- Lohner Milán/1I.py
+++ Lohner Milán/1I.py
@@ -1,4 +1,9 @@
+
 ido=input('Add meg az időpontot')
 ora, perc=ido.split(':')
-print(f'A beírt idő: {ora}:{perc}')
+ora=int(ora)
+perc=int(perc)
 
+kis_alfa = perc * 6
+nagy_alfa = (ora % 12) / 12 * 36
+print(f'{ora}:{perc}-kor a két mutató között {abs(nagy_alfa - kis_alfa)} fokos szög van')
