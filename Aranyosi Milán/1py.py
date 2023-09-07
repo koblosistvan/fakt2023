611acfd822e36dd6bb06e193c769cf697e969da2
aranyosimilan <cementes8@gmail.com>
2023-09-07 12:23:26 +0200

    kezdés

--- Aranyosi Milán/1py.py
+++ Aranyosi Milán/1py.py
@@ -1,3 +1,9 @@
 idő = input('Add meg az időpontot:')
 print(idő.split(':'))
-print(f'A beírt idő: {óra} : {perc}')
+óra, perc = idő.split(':')
+óra = int(óra)
+perc = int(perc)
+kis_alfa = perc * 6
+nagy_alfa = (óra % 12) * 30
+
+print(f'A beírt idő: {óra} : {perc} -kor a két mutató között {nagy_alfa - kis_alfa} fokos szög van')
