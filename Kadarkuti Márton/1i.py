737a85f9d593d44bb7de9e131e1afabbb15c3e47
merkur07 <marton.kadarkuti@gmail.com>
2023-09-07 12:21:40 +0200

    1i

--- Kadarkuti Márton/1i.py
+++ Kadarkuti Márton/1i.py
@@ -1,2 +1,9 @@
 ido = input("Add meg az időpontot: ")
 ora, perc = ido.split(":")
+
+ora, perc = int(ora), int(perc)
+
+kis_alfa = perc * 6
+nagy_alfa = (ora % 12) * 30
+
+print(f'{ora}:{perc}-kor a két mutató között {abs(nagy_alfa - kis_alfa)} fokos szög van')
