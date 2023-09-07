df12b6890daea39367b5ad8966b0cc3671105b44
callisto01 <bakcsenge@gmail.com>
2023-09-07 12:03:18 +0200

    idő

---
+++ Bak Csenge/1i.py
@@ -0,0 +1,14 @@
+#idő = input("Kérlek add meg az időpontot: ")
+idő = "10:00"
+
+
+óra, perc = idő.split(":")
+
+print(f"A beírt idő {óra}:{perc}")
+
+
+
+
+
+
+
---
+++ Bak Csenge/probálgatás.py
@@ -0,0 +1,21 @@
+print(f"5*2 = {5*2}")
+print(4 + 2 * 5)
+print("5" * 2222)
+print("AB" + "2")
+print(10/2)
+print(10//2)
+print(11//2)
+print(11 % 2)
+print(11 ** 2)
+print(11 ** 0.5)
+
+#a = int(input("Egyik szám: "))   #str = szöveg, int = szám, float = tört
+#b = int(input("Másik szám: "))
+
+#print(f"{a} + {b} = {a+b}")
+
+
+
+
+
+
