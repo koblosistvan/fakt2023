e071b9294d286fef1b28ab2849e12505be5f02b7
psemm <bakcsenge@gmail.com>
2023-09-10 18:13:22 +0200

    2nb

---
+++ Bak_Csenge/2b.py
@@ -0,0 +1,13 @@
+a = 2
+b = 8
+
+while True:
+    b_összeg = int(input("Mennyi a két szám összege? "))
+
+    if b_összeg == a + b:
+        print("Jap")
+        break
+    elif abs(b_összeg - (a + b) < 10):
+        print("Közel.")
+    else:
+        print("Nop")
