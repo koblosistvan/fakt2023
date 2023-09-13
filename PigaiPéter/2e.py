60d7f924860b45ecc5f5209858e028970db511e5
merkur13 <pigaipeter@gmail.com>
2023-09-13 11:27:59 +0200

    .

--- PigaiPéter/2e.py
+++ PigaiPéter/2e.py
@@ -1,7 +1,7 @@
 pont = input('Add meg a pontot:')
-andor = pont.replace('A','').replace('(','').replace(')' , '')
-x , y = andor.split(':')
-x , y = int(x) , int(y)
+andor = pont.replace('A', '').replace('(', '').replace(')', '')
+x, y = andor.split(':')
+x, y = int(x), int(y)
 
 if x > 0 and y > 0:
     print(f'{pont} pont az 1. síknegyedbe helyezkedik el')
@@ -9,17 +9,17 @@ if x > 0 and y > 0:
 elif x < 0 and y < 0:
     print(f'{pont} pont a 3. síknegyedbe helyezkedik el')
 
-elif x < 0 and y > 0:
+elif x < 0 < y:
     print(f'{pont} pont a 2. síknegyedbe helyezkedik el')
 
-elif x > 0 and y < 0:
+elif x > 0 > y:
     print(f'{pont} pont az 4. síknegyedbe helyezkedik el')
 
 elif x == 0 and y > 0:
-    print(f'{} pont illeszkeszkedik az x tengelyre')
+    print(f'{pont} pont illeszkeszkedik az x tengelyre')
 
 elif x > 0 and y == 0:
-    print(f'{} pont illeszkeszkedik az y tengelyre')
+    print(f'{pont} pont illeszkeszkedik az y tengelyre')
 
 else:
     print(f'{pont} az origóra illeszkedik')
