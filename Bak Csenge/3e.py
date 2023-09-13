0aebb794061255bd96e0c78b1602ce5494a99448
psemm <bakcsenge@gmail.com>
2023-09-13 20:47:45 +0200

    Hf

---
+++ Bak Csenge/3e.py
@@ -0,0 +1,28 @@
+Pí = 1
+a = 3
+
+
+for i in range(1, 1000000):
+    if i % 2 != 0:
+        Pí -= (1 / a)
+    else:
+        Pí += (1 / a)
+    a += 2
+
+print(Pí, end="\n\n")
+
+while a < 1000000:
+    Pí += (1 / a)
+    a += 2
+    Pí -= (1 / a)
+    a += 2
+print(Pí)
+
+
+
+
+
+
+
+
+
