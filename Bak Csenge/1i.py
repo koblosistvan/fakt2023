97dfa4ea041d31b8fe371da217d4a3ce07bdf5da
callisto01 <bakcsenge@gmail.com>
2023-09-13 11:27:21 +0200

    idő

--- Bak Csenge/1i.py
+++ Bak Csenge/1i.py
@@ -1,10 +1,14 @@
 #idő = input("Kérlek add meg az időpontot: ")
-idő = "10:00"
-
+idő = "02:15"
 
 óra, perc = idő.split(":")
+óra = int(óra)
+perc = int(perc)
 
-print(f"A beírt idő {óra}:{perc}")
+kis_afa = perc/60 * 360
+nagy_alfa = (óra % 12) / 12 * 360
+
+print(f"{óra}:{perc}-kor a két mutató között {abs(nagy_alfa - kis_afa)} fokos szög van.")
 
 
 
