23b0638aa352eaa9a58b1ecfc7d116b411c59c22
psemm <bakcsenge@gmail.com>
2023-09-17 13:32:50 +0200

    Hf

--- Bak Csenge/3g.py
+++ Bak Csenge/3g.py
@@ -1,7 +1,6 @@
 import random
 
-random = random.randint(0,2)
-k_p_o = ["kő", "papír", "oló"]
+k_p_o = ["kő", "papír", "olló"]
 
 én = 0
 gép = 0
@@ -10,14 +9,55 @@ kő = ["olló", "kő"]
 papír = ["papír", "kő"]
 olló = ["papír", "olló"]
 
-lista = []
 
 
-while én < 3 or gép < 3:
-    input_e = input(" ")
-    print(k_p_o[random])
-    lista.append(input_e, k_p_o[random])
-    if lista == kő:
+print('Ha ki szeretnél lépni, akkor írd be a "k" betűt!\n')
+while én < 3 and gép < 3:
+    létezik = True
+    izé = random.randint(0, 2)
+    input_e = input("")
+    if input_e != "kő" and input_e != "papír" and input_e != "olló" and input_e != "k" and input_e.find("basszus") != -1 :
+        print("A beírt szöveg nem értelmezhető, kérlek próbáld újra!\n")
+        létezik = False
+    if input_e.find("basszus") != -1:
+        print("Ne káromkodj, ezért büntit kapsz >:C")
+        én -= 1
+        print(f"Állás: {én = }\t\t{gép = } \n")
+        létezik = False
+    if létezik:
+        print(k_p_o[izé])
+    if input_e == "k":
+        if input("Biztos ki szeretnél lépni?(Jap - igen, Nop - nem) ") == "Jap":
+            print("Bénaaaa, feladtad")
+            én = 0
+            gép = 3
+            break
+        else:
+            print("\nFigyelj már oda mit írsz!! >:c ")
+            print(f"Na jóvan, ha annyira szeretnéd folytatni, folytasd.... Az aktuális állás a következő: {én=} {gép=}")
+    if [input_e, k_p_o[izé]] == kő or [k_p_o[izé], input_e] == kő:
+        if input_e == "kő":
+            én += 1
+        else:
+            gép += 1
+    elif [input_e, k_p_o[izé]] == papír or [k_p_o[izé], input_e] == papír:
+        if input_e == "papír":
+            én += 1
+        else:
+            gép += 1
+    elif [input_e, k_p_o[izé]] == olló or [k_p_o[izé], input_e] == olló:
+        if input_e == "olló":
+            én += 1
+        else:
+            gép += 1
+    if létezik:
+        print(f"Állás: {én=} {gép=} \n")
+
+
+if én > gép:
+    print("Én győztem! Hehee :DD")
+else:
+    print("A gép győzött :'C")
 
 
 
