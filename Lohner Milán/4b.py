1b863b33acd01bde54d7a8a5b6f170b4e206bb8a
milan <milan.lohner10@gmail.com>
2023-09-23 18:03:07 +0200

    1

--- Lohner Milán/4b.py
+++ Lohner Milán/4b.py
@@ -19,13 +19,20 @@ szazalekok = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 szazalek_szeletek = [f"{szazalek}-{szazalek+9}%" for szazalek in szazalekok]
 
-dolgozatok_szama_szeletekben = [sum(1 for pont in pontok if pont >= PONTHATAR + szazalek and pont <= PONTHATAR + szazalek + 9) for szazalek in szazalekok]
+dolgozatok_szama_szeletekben = [0] * len(szazalekok)
+
+for pont in pontok:
+    for i, szazalek in enumerate(szazalekok):
+        if pont >= PONTHATAR + szazalek and pont <= PONTHATAR + szazalek + 9:
+            dolgozatok_szama_szeletekben[i] += 1
 
 print("Pontok:")
 for i, pont in enumerate(pontok):
     print(f"Dolgozat {i + 1}: {pont} pont")
 
+
 for i, szazalek_szelet in enumerate(szazalek_szeletek):
-    print(f"{szazalek_szelet} sáv: {dolgozatok_szama_szeletekben[i]} dolgozat")
+    if dolgozatok_szama_szeletekben[i] > 0:
+        print(f"{szazalek_szelet} sáv: {dolgozatok_szama_szeletekben[i]} dolgozat")
 
 print(f'Ebbol {jeles} db lett jeles es {haromnal_kevesebb} olyan osztalyzat volt ahol csak 3 pontnal kevesebb hianyzott, {tokeletes}db tokeletes erettsegi szuletett')
