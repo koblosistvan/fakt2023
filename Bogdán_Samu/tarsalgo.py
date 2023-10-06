b1faea5a42abee1b25f5ae13fca2c4da98ba0e1e
B-Samu <dacinator69@gmail.com>
2023-10-06 14:46:25 +0200

    feladatok

---
+++ Bogdán_Samu/tarsalgo.py
@@ -0,0 +1,32 @@
+forrás = open('ajto.txt', mode='r', encoding='utf-8')
+
+óra = []
+perc = []
+az = []
+log = []
+for i in forrás:
+    sor = i.strip().split(' ')
+    óra.append(int(sor[0]))
+    perc.append(int(sor[1]))
+    az.append(int(sor[2]))
+    log.append(str(sor[3]))
+
+index_be = []
+index_ki = []
+for i in range(len(log)):
+    if log[i] == 'be':
+        index_be.append(i)
+    if log[i] == 'ki':
+        index_ki.append(i)
+index_be = index_be[0]
+index_ki = index_ki[-1]
+print(az[index_be], az[index_ki])
+
+athaladas = open('athaladas.txt', mode='w', encoding='utf-8')
+azonosító = list(dict.fromkeys(az))
+azonosító.sort()
+
+for i in azonosító:
+    pass
+
+forrás.close()
