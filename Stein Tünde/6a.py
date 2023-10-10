17eb50f6047b8fd634a31d186e76d83dc1f20f77
Stein Tünde <tundes909@gmail.com>
2023-10-10 18:39:46 +0200

    ~

--- Stein Tünde/6a.py
+++ Stein Tünde/6a.py
@@ -14,5 +14,14 @@ for sor in forras:
     szuletes.append(szuletesi_datum)
     kezdesek.append(int(adat[3]))
     vegzesek.append(int(adat[4]))
-
-
+forras.close()
+bekert_evszam = int(input('Adj meg egy évszám: '))
+bekert_datum = input('Add meg a születési dátumodat: ')
+bekert_datum.strip().split('. ')
+int(bekert_datum)
+print(bekert_datum)
+for i in range(len(vegzesek)):
+    if bekert_evszam < vegzesek[i]:
+        print('Van olyan hallgató, aki ennél fiatalabban szerzett diplomát.')
+    else:
+        print('Nincs olyan hallgató, aki ennél fiatalabban szerzett diplomát.')
