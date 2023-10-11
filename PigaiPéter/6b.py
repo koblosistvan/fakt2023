ba30db35607da8a3fdfbbba77854a4fb6bcdd511
Pigusz <144223753+Pigusz@users.noreply.github.com>
2023-10-11 22:21:04 +0200

    Update 6b.py
    .

--- PigaiPéter/6b.py
+++ PigaiPéter/6b.py
@@ -1,32 +1,88 @@
 import cuccok
 forrás = open('6b.txt', mode='r', encoding='utf8')
 cuccok.vonal()
+
+forrás = open('6b-forgalom.txt', mode='r', encoding='utf-8')
+
 hely = []
-idopont = []
+időpont = []
 
 forrás.readline()
 for sor in forrás:
     adat = sor.strip().split(' ')
     hely.append(int(adat[0]))
-    idopont.append(int(adat[1]) * 60 + int(adat[2]))
+    időpont.append(int(adat[1])*60 + int(adat[2]))
 forrás.close()
 
+def óra_perc(i):
+    időérték = időpont[i]
+    óra = időérték // 60
+    perc = időérték % 60
+    return f'{óra:0>2d}:{perc:0>2d}'
+
+def óra_perc2(i):
+    return f'{időpont[i]//60}:{időpont[i]/60}'
+
+print('-' * 30)
 leghosszabb_id = 0
-leghosszabb_ido = idopont[1] - idopont[0]
+leghosszabb_idő = időpont[1] - időpont[0]
 for i in range(len(hely)-1):
-    if idopont[i+1] - idopont[i] > leghosszabb_ido:
+    if időpont[i+1] - időpont[i] > leghosszabb_idő:
         leghosszabb_id = i
-        leghosszabb_ido = idopont[i+1] - idopont[i]
+        leghosszabb_idő = időpont[i+1] - időpont[i]
 
-ötvenespontok = 0
+print(f'A leghosszabb szünet {leghosszabb_idő} perc volt {óra_perc(leghosszabb_id)} és {óra_perc(leghosszabb_id+1)} között.')
+print('Vége.')
+
+otvenes = 0
+
 for i in range(len(hely)):
-    if hely[i] == 50:
-        ötvenespontok += 1
+    if i == 50:
+        otvenes += 1
+print(f'{otvenes}szer van otvenes helyen meres')
 
-print(f'{ötvenespontok} adat tartozik az 50es mérési ponthoz')
-cuccok.vonal()
+bekert = int(input('adj meg egy idopontot'))
+van = False
+for i in range(len(idopont)):
+    if i == bekert:
+        van = True
+if van:
+    print('van ilyen idopont')
+else:
+    print('nincs ilyen idopont')
 
-bekért = int(input('időpont:'))
+volt = False
+for i in range(len(idopont)-1):
+    if idopont[i] == idopont[i+1]:
+        volt = True
+if volt:
+    print('volt azonos idopontban tobb meres')
+else:
+    print('nem volt azonos idopontban meres')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
