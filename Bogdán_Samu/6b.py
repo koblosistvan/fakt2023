df3b60fa49095dd573ac25a9f916feaf9812317c
callisto06 <callisto06@server>
2023-10-11 11:58:32 +0200

    feladatok

--- Bogdán_Samu/6b.py
+++ Bogdán_Samu/6b.py
@@ -1,17 +1,14 @@
 forrás = open('6b-forgalom.txt', mode='r')
 a = forrás.readline().strip().split(' ')
-helyek = a[0]
+hely_db = a[0]
 adatok = a[1]
 hely = []
 időpont = []
-
 for i in forrás:
     adat = i.strip().split(' ')
     hely.append(int(adat[0]))
     időpont.append(int(adat[1])*60 + int(adat[2]))
 forrás.close()
-
-
 leghosszabb_id = 0
 leghosszabb_idő = időpont[1] - időpont[0]
 for i in range(len(hely)-1):
@@ -24,4 +21,12 @@ for i in range(len(hely)):
     if hely[i] == 50 and (időpont[i] // 60 < 6):
         hatelott50 += 1
 print(hatelott50)
+óraperc = str(input('Adjon meg egy időpontot "óra:perc" formátumban! '))
+óra = int(óraperc.split(':')[0])
+perc = int(óraperc.split(':')[1])
+if (óra * 60 + perc) in időpont:
+    print('Volt mérési eredmény ebben az időpontban.')
+else:
+    print('Nem volt mérési eredmény ebben az időpontban.')
+helyek = set(hely)
 
