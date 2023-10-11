c750d202a3c1af611a0a2b2c47902748c0a1141b
merkur13 <pigaipeter@gmail.com>
2023-10-11 12:43:27 +0200

    .

--- PigaiPéter/cuccok.py
+++ PigaiPéter/cuccok.py
@@ -8,3 +8,13 @@ def prime(szam):
             a = False
     return a
 
+def andor(x):
+    a = '' + 'I'
+    for i in range(1, x + 1):
+        a += 'I'
+        print(f'{a} + 1')
+def soos(b):
+    a = '' + 'I'
+    for i in range(1, b + 1):
+        print(f'elif {a} == i:\n\t\t {a} += 1')
+        a += 'I'
