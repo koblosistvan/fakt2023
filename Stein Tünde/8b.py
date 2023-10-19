23ad8a23a6440ff726a77dda878a8ace7e3ea997
Stein Tünde <tundes909@gmail.com>
2023-10-19 17:25:23 +0200

    ~

--- Stein Tünde/8b.py
+++ Stein Tünde/8b.py
@@ -9,3 +9,11 @@ for sor in forras:
     lakasok_ar.append(int(adat[1]))
 for i in range(0, len(lakasok_alapterulet), - 1):
     print(lakasok_ar)
+
+
+lista = [1, 2, 3, 4, 67, 0]
+lista2 = []
+for i in range(len(lista) - 1):
+    if lista[i] > lista[i+1]:
+        lista2.append(lista[i])
+        i_erteke = i
