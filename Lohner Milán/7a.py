acf8a8b14b7c0b53118d481d409cee865656ca50
milan <milan.lohner10@gmail.com>
2023-10-23 18:54:02 +0200

    1

--- Lohner Milán/7a.py
+++ Lohner Milán/7a.py
@@ -1,14 +1,16 @@
 forras = open('7a-lakas-arak.txt', mode='r', encoding='utf-8')
 
+darab = int(forras.readline())
+
 terulet = []
 
 ar = []
 
-forras.readline()
+
 for sor in forras:
     adat = sor.strip().split(' ')
-    ar.append(int(adat[0]))
-    terulet.append(int(adat[1]))
+    ar.append(int(adat[1]))
+    terulet.append(int(adat[0]))
 
 
 
@@ -16,11 +18,27 @@ forras.close()
 
 
 legdragabb_ar = max(ar)
-legdragabb_id =
+
+print(f'A legdragabb haz ara {legdragabb_ar} millio ft', f'es az a haz a {ar.index(max(ar))}')
+
+
+vanlakas = False
 
 for i in range(len(ar)):
+    if ar[i] > 500:
+        vanlakas = True
+        break
 
 
 
+if vanlakas == True:
+    print('Van olyan lakas')
+else: print('Nincs olyan lakas')
+
+
+aroszto = []
+
+
+for i in range(len(darab))
 
 
