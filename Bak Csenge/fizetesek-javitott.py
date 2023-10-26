8cc3d05cf0dc458d0d0138593f695f9c237db3e8
callisto01 <bakcsenge@gmail.com>
2023-10-26 12:30:16 +0200

    órai

--- Bak Csenge/fizetesek-javitott.py
+++ Bak Csenge/fizetesek-javitott.py
@@ -25,10 +25,14 @@ forrás.close()
 # 2. feladat: hány adatsort tartalmaz az adatfájl?
 # minta: "A fájl 18 sort tartalmaz."
 
+fel(2)
+
 print(f"A fájl {len(év)} sort tartalmaz.")
 
 # 3. feladat: hány esetben csökkent a fizetés a tapasztalat növekedésével?
 # minta: "5 esetben csökkent az átlagfizetés."
+
+fel(3)
 
 csökkent_a_fizu = 0
 
@@ -42,11 +46,15 @@ print(f"{csökkent_a_fizu} esetben csökkent az átlagfizetés.")
 # minta: "a Legmagasabb fizetést 7,8 év munkatapasztalattal lehet kapni."
 # figyelj arra, hogy a minta szerinti legyen a szám megjelenítése
 
+fel(4)
+
 print(f"a legmagasabb fizetést {év[kereset.index(max([kereset[i] for i in range(len(kereset))]))]} év munkatapasztalattal lehet kapni.")
 
 # 5. feladat: listázd ki, hogy mely sorok jelentenek visszaeseést a fizetésben!
 # minta: "4,2 év: $56 547"
 # minta extra: "4,1 -> 4,2 év: $57 247 -> $56 547 (-1 300)"
+
+fel(5)
 
 for i in range(1, len(év)):
 	if év[i-1] < év[i] and kereset[i-1] > kereset[i]:
