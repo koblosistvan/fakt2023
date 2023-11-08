85a0c12aa5e7dd57e50936a4f65df2fd8a6c240e
aranyosimilan <cementes8@gmail.com>
2023-11-08 22:53:08 +0100

    .

--- Aranyosi Milán/KOMAL I600.py
+++ Aranyosi Milán/KOMAL I600.py
@@ -1,12 +1,16 @@
-oszlopok_szama = int(input('Az oszlopok száma: '))
+oszlopok_szama = int(input('Add meg hány oszlop van:'))
 
-oszlopok = [
-
-]
+oszlopok = []
+haszn_korongok = []
+szabad_korongok = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
 for i in range(oszlopok_szama):
-    asd = int(input('Az oszlop elemei: '))
-    oszlop = []
-    for j in range(asd):
-        adat = j.split(' ')
-        oszlop.append(adat)
+    sor = input(f'{i+1}. oszlop: ').strip().split(' ')
+    for i in range(len(sor)):
+        sor[i] = int(sor[i])
+    haszn_korongok = haszn_korongok + sor
+    oszlopok.append(sor)
+
+for j in range(len(haszn_korongok)):
+    if szabad_korongok[j] in haszn_korongok:
+        szabad_korongok.remove(int(haszn_korongok[j]))
