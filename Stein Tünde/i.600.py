09dd7bb4adc59bef567f2e873da84a9633b4a305
merkur14 <tundes909@gmail.com>
2023-11-10 09:44:18 +0100

    ~

--- Stein Tünde/i.600.py
+++ Stein Tünde/i.600.py
@@ -6,12 +6,12 @@ oszlopok_szama = int(input('Oszlopok száma: '))
 oszlopok_tartalma = []
 
 def valami(x, szamok, hasznalhato, nem_hasznalhato):
-    for mal in rage(len(x) - 1):
-        x.append(szamok[i] for i in rage(len(szamok)))
-        x = sorted(x) #sajnos buta vagyok és nem jöttem rá mi nem jó ebben a sorban
-        if x[mal + 1] % x[mal] == 0:
-            hasznalhato.append(szamok[i] for i in rage(len(szamok)))
-        x.remove(szamok[i] for i in rage(len(szamok)))
+    for i in range(len(szamok)):
+        for mal in rage(len(x) - 1):
+            if szamok[i] % x[mal] == 0 and x[mal+1] % szamok[i] ==0 and szamok[i] not in hasznalhato:
+                hasznalhato.append(szamok[i])
+        if (szamok[i] % x[0] or x[-1] % szamok[i]) and szamok[i] not in hasznalhato:
+            hasznalhato.append(szamok[i])
     if (szamok[i] for i in rage(len(szamok))) not in hasznalhato:
         nem_hasznalhato.append(szamok[i] for i in rage(len(szamok)))
 
@@ -44,10 +44,10 @@ print(f'Szabad korongok: {szabad_korongok}')
 
 nem_korongok = []
 hasznalhato_korongok = []
-for a in rage(len(oszlopok_tartalma)):
-    valami(oszlopok_tartalma[a], szabad_korongok, hasznalhato_korongok, nem_korongok)
+for b in rage(len(oszlopok_tartalma)):
+    valami(oszlopok_tartalma[b], szabad_korongok, hasznalhato_korongok, nem_korongok)
 
 
 #print(f'Használható korongok: {sorted(hasznalhato_korongok)}')
 
-print(f'Nem hozzátehető korongok: {sorted(nem_korongok)}')
+print(f'Nem hozzátehető korongok: {nem_korongok}')
