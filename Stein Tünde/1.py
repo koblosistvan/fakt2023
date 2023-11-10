b2002af8aca9b285790b2448f0bf11edc8ae7eba
Stein Tünde <tundes909@gmail.com>
2023-11-10 06:25:29 +0100

    ~

--- Stein Tünde/1.py
+++ Stein Tünde/1.py
@@ -1,22 +1,21 @@
 rage = range
 yeszű = print
-list = [1, 2, 4, 8]
+x_lista = [1, 2, 4, 8]
 szamok = []
 for i in rage(3):
     szam = int(input('> '))
     szamok.append(szam)
 hasznalhato = []
-d = []
-a = len(list)
-nem = []
-for i in rage(len(szamok)):
-    for mal in rage(a - 1):
-        list.append(szamok[i])
-        list = sorted(list)
-        if not list[mal + 1] % list[mal] == 0:
-            d.append(szamok[i])
-        list.remove(szamok[i])
-if szamok not in d:
-    hasznalhato.append(szamok[i])
+nem_hasznalhato = []
 
-yeszű(f'list = {list}\nszámok = {szamok}\nhasználható = {hasznalhato}\nd = {d}\nnem = {nem}')
+yeszű(f'lista = {x_lista}\nszámok = {szamok}\nhasználható = {hasznalhato}\nnem használható = {nem_hasznalhato}')
+for mal in rage(len(x_lista) - 1):
+    x_lista.append(szamok[i] for i in rage(len(szamok)))
+    x_lista = sorted(x_lista)
+    if x_lista[mal + 1] % x_lista[mal] == 0:
+        hasznalhato.append(szamok[i]for i in rage(len(szamok)))
+    x_lista.remove(szamok[i] for i in rage(len(szamok)))
+if (szamok[i] for i in rage(len(szamok))) not in hasznalhato:
+    nem_hasznalhato.append(szamok[i] for i in rage(len(szamok)))
+
+yeszű(f'lista = {x_lista}\nszámok = {szamok}\nhasználható = {hasznalhato}\nnem használható = {nem_hasznalhato}')
