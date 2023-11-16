7382b8d2d1feb9fd6db245c876807f716b059105
uranusz26 <milan.lohner10@gmail.com>
2023-11-16 12:41:02 +0100

    1

--- Lohner Milán/hajok.py
+++ Lohner Milán/hajok.py
@@ -16,17 +16,23 @@ for sor in forras:
     celallomasok.append(sor.strip())
 
 
-városok = set(celallomasok)
-
+városok = set(sorted(celallomasok))
+hajok = []
 
 for város in városok:
     konténer_varosba = celallomasok.count(város)
-    ureshelyek =
+    hajokvarosba = math.ceil(konténer_varosba/kontenerperhajo)
 
-    print(f'{város} {math.ceil(konténer_varosba/kontenerperhajo)}, ureshelyek')
+    print(f'{város} {math.ceil(konténer_varosba/kontenerperhajo)}, {hajokvarosba-konténer_varosba}')
+    hajok.append([város, hajokvarosba, hajokvarosba*kontenerperhajo-konténer_varosba])
+
+for i in range(len(hajok)):
+    for j in range(len(hajok)):
+        if hajok[i][2] > hajok[j][2] or (hajok[i][2] == hajok[j][2] and hajok[i][1] > hajok[j][1]):
+            hajok[i], hajok[j] = hajok[j], hajok[i]
+
+#print(hajok)
 
 
-
-
-
-
+for i in range(3):
+    print(f'{hajok[i][0]}, {hajok[i][1]}, {hajok[i][2]}')
