aed27a23a615d1271b7cdd0a429412ebc90068e9
B-Samu <dacinator69@gmail.com>
2023-11-22 11:20:32 +0100

    feladatok

---
+++ Bogdán_Samu/bumm.py
@@ -0,0 +1,9 @@
+forrás = input('bumm(ettől,eddig,tiltott): ')
+ettől = int(forrás.split(',')[0])
+eddig = int(forrás.split(',')[1])
+tiltott = int(forrás.split(',')[2])
+for i in range(ettől, eddig+1):
+    if str(tiltott) in list(str(i)) or i % tiltott == 0:
+        print('!!')
+    else:
+        print(i)
