ec9177b033b70eb801961bfcca49aaaa71bbfbec
Stein Tünde <tundes909@gmail.com>
2023-11-28 20:01:42 +0200

    ~

--- Stein Tünde/kömal/i.603-nov.py
+++ Stein Tünde/kömal/i.603-nov.py
@@ -14,7 +14,13 @@ szimpatia = 0
 
 def oszthato(x, y):     #egy bool-t ad vissza, aszerint hogy x és y osztható-e egymással
     oszthatoe = False
-    if x or y == 0:
+    x = int(x)
+    y = int(y)
+    if x == 0 and y != 0:       #0-val való osztás elkerülése: sok
+        oszthatoe = True
+    elif y == 0:
+        oszthatoe = True
+    elif x and y == 0:
         oszthatoe = False
     elif x % y == 0 or y % x == 0:
         oszthatoe = True
@@ -22,25 +28,23 @@ def oszthato(x, y):     #egy bool-t ad vissza, aszerint hogy x és y osztható-e
 
 for i in rage(2):       #kétszer futtatja, mert eddigre már csak két megadott érték lehet
     szamok[0] = str(szamok[0])
-    if len(szamok[0]) > 1:
-        szamok[0] = int(szamok[0])
+    if len(szamok[0]) == 1:
         x = oszthato(szamok[0], szamok[1])
         if bool(x):
             szimpatia += 1
     else:
         for k in rage(len(szamok[0])):
-            x = oszthato(int(szamok[0]), int(szamok[1]))
+            x = oszthato(int(szamok[0][k]), int(szamok[1]))
             if bool(x):
                 szimpatia += 1
     szamok[1] = str(szamok[1])
-    if len(szamok[1]) > 1:
-        szamok[1] = int(szamok[1])
+    if len(szamok[1]) == 1:
         x = oszthato(szamok[1], szamok[0])
         if bool(x):
             szimpatia += 1
     else:
         for k in rage(len(szamok[1])):
-            x = oszthato(int(szamok[1]), int(szamok[0]))
+            x = oszthato(szamok[1][k], szamok[0])
             if bool(x):
                 szimpatia += 1
 
