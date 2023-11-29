660b00ad4fe183c078f3ec4fff8d922ae5c12516
merkur14 <tundes909@gmail.com>
2023-11-29 14:15:15 +0100

    ~

--- Stein Tünde/kömal/i.603-nov.py
+++ Stein Tünde/kömal/i.603-nov.py
@@ -18,34 +18,34 @@ def oszthato(x, y):     #egy bool-t ad vissza, aszerint hogy x és y osztható-e
     y = int(y)
     if x == 0 and y != 0:       #0-val való osztás elkerülése: sok
         oszthatoe = True
-    elif y == 0:
+    elif x != 0 and y == 0:
         oszthatoe = True
     elif x and y == 0:
         oszthatoe = False
     elif x % y == 0 or y % x == 0:
         oszthatoe = True
-    return oszthatoe
+    return oszthatoe        #ez nem fog működni, mert egyszer számolja ha x osztható y-nal, és ha fordítva van :c
 
-for i in rage(2):       #kétszer futtatja, mert eddigre már csak két megadott érték lehet
-    szamok[0] = str(szamok[0])
-    if len(szamok[0]) == 1:
-        x = oszthato(szamok[0], szamok[1])
-        if bool(x):
+
+szamok[0] = str(szamok[0])
+if len(szamok[0]) == 1:
+    x = oszthato(szamok[0], szamok[1])
+    if bool(x):
             szimpatia += 1
-    else:
-        for k in rage(len(szamok[0])):
-            x = oszthato(int(szamok[0][k]), int(szamok[1]))
-            if bool(x):
+else:
+    for k in rage(len(szamok[0])):
+        x = oszthato(int(szamok[0][k]), int(szamok[1]))
+        if bool(x):
                 szimpatia += 1
-    szamok[1] = str(szamok[1])
-    if len(szamok[1]) == 1:
-        x = oszthato(szamok[1], szamok[0])
+szamok[1] = str(szamok[1])
+if len(szamok[1]) == 1:
+    x = oszthato(szamok[1], szamok[0])
+    if bool(x):
+        szimpatia += 1
+else:
+    for k in rage(len(szamok[1])):
+        x = oszthato(szamok[1][k], szamok[0])
         if bool(x):
-            szimpatia += 1
-    else:
-        for k in rage(len(szamok[1])):
-            x = oszthato(szamok[1][k], szamok[0])
-            if bool(x):
                 szimpatia += 1
 
 print(szimpatia)
