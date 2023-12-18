c44cbd6ad34813f9ddc42d268b98246bebcc2f05
koblosmandula <koblosmandula@gmail.com>
2023-12-18 14:59:06 +0100

    kész

---
+++ Köblös Mandula/_Dolgozat/cooper.py
@@ -0,0 +1,47 @@
+# olvasd be az adatokat tartalmazó fájlok egyikét
+# 	a cooper.txt minden számadatot tartalmaz, de a neveket nem, ezzel lesz egyszerűbb dolgoznod
+# 	a cooper-extra.txt a neveket és egy fejlécet is tartalmaz, ezzel oldhatod meg az extra feladatokat is
+# --------------------------------------------------------------------------------------------------------
+# mindkét fájl a 11. évfolyam Cooper tesztjének eredményét tartalmazza, azaz hogy tavaly és idén 
+# hány métert tudtak futni a diákok 12 perc alatt
+# a programodat a megjegyzésként beírt szövegrészek után (közé) írd be
+# figyelj a minta szövegre, annak megfelelően formázd a saját kiírásaidat is
+
+# --------------------------------------------------------------------------------------------------------
+# 1. feladat: adatok beolvasása és tárolása
+
+
+# --------------------------------------------------------------------------------------------------------
+# 2. feladat: hány diák vett részt a teszten
+# minta: A teszten 12 diák vett részt.
+
+
+# --------------------------------------------------------------------------------------------------------
+# 3. feladat: hány diák futott legalább 3000 m-t idén?
+# minta: Idén 3 diák futott legalább 3000 m-t.
+
+
+# --------------------------------------------------------------------------------------------------------
+# 4. feladat: mennyi volt a legjobb futó eredménye idén?
+# minta: Az idei legjobb eredmény 3450 m volt.
+# minta-extra: Az idei legjobb eredményt Mák Áron érte el 3450 m-es távval.
+
+
+# --------------------------------------------------------------------------------------------------------
+# 5. feladat: mennyi volt a legnagyobb javítás (azaz az idei-tavalyi eredmény maximális értéke)?
+# minta: A legnagyobb javítás 231 m volt.
+# minta-extra: A legtöbbet Gáz Áron javított, ő 265 m-rel futott többet idén, mint tavaly.
+
+
+# --------------------------------------------------------------------------------------------------------
+# 6. feladat: listázd ki az idei 3000 m felett teljesítőket
+# minta: 
+# 3124
+# 3187
+# ...
+# minta-extra: 
+# Mekk Elek    3143
+# Kis Miska    3251
+# Kő Pál       3423
+# ...
+
