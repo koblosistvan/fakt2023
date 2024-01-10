5732e5ecdfe1c37dcae383a18bc2a71955ab8a03
uranusz26 <milan.lohner10@gmail.com>
2024-01-10 11:46:57 +0100

    1

---
+++ Lohner Milán/sieles_feladat.py
@@ -0,0 +1,9 @@
+# 1. Hány lesiklása volt aznap?
+
+# 2. Mekkora távolságot tett meg összesen ezalatt?
+
+# 3. Volt-e 2000 m-nél hosszabb lesiklása?
+
+# 4. Mikor kezdődött a leggyorsabb lesiklás?
+
+# 5. Listázd ki a lesiklas.txt fájlba, azokat a lesiklásokat, melyk 4 percnél tovább tartottak!
