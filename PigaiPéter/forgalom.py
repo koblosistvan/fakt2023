a14a00488e67864e693ecb61cc7f6e363960ea7e
Pigusz <pigaipeter@gmail.com>
2024-04-25 12:36:10 +0200

    .

--- PigaiPéter/forgalom.py
+++ PigaiPéter/forgalom.py
@@ -6,11 +6,19 @@ class Meres:
         self.perc = perc
         self.mp = mp
         self.idotartam = idotartam
-        self.sebesseg = 3600/idotartam
+        self.sebesseg = 1000/idotartam
         self.honnan = honnan
+        self.honnanstr = 'Felső' if honnan == 'F' else 'Alsó'
         self.hova = "Felső város" if honnan == "A" else "Alsó város"
     def ido_mp(self):
         return self.ora * 3600 + self.perc * 60 + self.mp
+    def __gt__(self, other):
+        if self.sebesseg > other.sebesseg:
+            return True
+        else:
+            return False
+    def idostr(self):
+        return(f'{self.ora:0>2d}:{self.perc:0>2d}:{self.mp:0>2d}')
 
 forgalom = []
 forras.readline()
@@ -22,4 +30,16 @@ forras.close
 n = int(input("Melyik jármű adataid kéred?"))
 print(f"Az {n}. jármű {forgalom[n-1].hova} felé haladt")
 
-print(f"Az utolsó két autó között {forgalom[-1].ido_mp() - forgalom[-2].ido_mp()}s telt el")
+felsofele = [meres for meres in forgalom if meres.honnan == 'A']
+
+print(f"Az utolsó két autó között {felsofele[-1].ido_mp() - felsofele[-2].ido_mp()}s telt el")
+
+for i in range(24):
+    felsofelol = [meres for meres in forgalom if meres.honnan == 'F' and meres.ora == i]
+    alsofelol = [meres for meres in forgalom if meres.honnan == 'A' and meres.ora == i]
+    if len(felsofelol) > 0 or len(alsofelol) > 0:
+        print(i, len(felsofelol), len(alsofelol))
+
+leggyorsabb = sorted(forgalom)
+for i in range(10):
+    print(f'{leggyorsabb[i].idostr()} {leggyorsabb[i].honnanstr} {leggyorsabb[i].sebesseg:.1f}')
