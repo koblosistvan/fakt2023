e463442b1c3bcea5d264c8f7f453a7b14912dd6a
merkur14 <tundes909@gmail.com>
2024-05-16 12:43:16 +0200

    ~

--- Stein Tünde/érettségi/imdb.py
+++ Stein Tünde/érettségi/imdb.py
@@ -48,7 +48,7 @@ for i in ertekeles:
 print(f'{max(ertekeles)} a legmagasabb értékelés')
 
 # task 6
-ertekeles2 = sorted(set(ertekeles))
+ertekeles2 = sorted(list(set(ertekeles)))
 ertekeles_lista = []
 for i in ertekeles2:
     ertekeles_lista.append(str(ertekeles.count(i)))
@@ -61,21 +61,60 @@ for i in range(len(ertekeles)):
 
 # task 8
 input_rendezo = input('> ')
-txt = open(f'{input_rendezo.split(" ")[0].lower}.txt', "w", encoding=utf-8)
+txt = open(f'{input_rendezo.split(" ")[0]}.txt', "w", encoding='utf-8')
 for i in range(len(rendezo)):
     if rendezo[i] == input_rendezo:
         print(cim[i], file=txt)
 
 # task 9
 ev2 = sorted(set(ev))
-evek = 0
 osszbevetel = 0
 for i in ev2:
+    evek = 0
     for k in range(len(ev)):
         if ev[k] == i:
             evek += 1
             osszbevetel += bevetel[k]
     print(f'{i}-ban/ben {evek} film készült, és {osszbevetel} volt a teljes bevétel.')
-    ev_lista.append(str(ev.count(i)))
-print(", ".join(ev_lista))
+
+# taskl 10
+print(f'Egy film átlagos bevétele: {sum(bevetel)/len(bevetel)}')
+
+# task 11
+print('A 10 legnagyobb bevételű film:')
+bevetel2 = bevetel
+cim2 = cim
+for i in range(len(bevetel2)-1):
+    if bevetel2[i] < bevetel2[i+1]:
+        bevetel2[i], bevetel2[i+1] = bevetel2[i], bevetel2[i+1]
+        cim2[i], cim2[i + 1] = cim2[i], cim2[i + 1]
+for i in range(10):
+    print(cim2[i])
+
+# task 12
+osszfilmek = []
+legtobb = 0
+for i in rendezo:
+    osszfilmek.append(rendezo.count(i))
+rendezo2 = rendezo
+for i in range(len(osszfilmek)-1):
+    if osszfilmek[i] < osszfilmek[i+1]:
+        osszfilmek[i], osszfilmek[i+1] = osszfilmek[i], osszfilmek[i+1]
+        rendezo2[i], rendezo2[i + 1] = rendezo2[i], rendezo2[i + 1]
+print('Az 5 legtöbb filmet rendező neve:')
+for i in range(len(rendezo2)):
+    ertekelesei = []
+    filmjei = []
+    for k in range(len(rendezo)):
+        if i == rendezo[k]:
+            ertekelesei.append(ertekeles[k])
+            filmjei.append(cim[k])
+    for k in range(len(ertekelesei) - 1):
+        if ertekelesei[k] < ertekelesei[k + 1]:
+            ertekelesei[k], ertekelesei[k + 1] = ertekelesei[k], ertekelesei[k + 1]
+            filmjei[k], filmjei[k + 1] = filmjei[k], filmjei[k + 1]
+    print(f'{i} - átlagértékelés: {sum(ertekelesei)/len(ertekelesei)}')
+    print('\t' "\n\t".join(filmjei))
+
+
 
