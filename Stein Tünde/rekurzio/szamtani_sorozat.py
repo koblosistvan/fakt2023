c7f07d9d076bfa5d008babbcb9a262b7fec0e23c
merkur14 <tundes909@gmail.com>
2024-05-22 12:41:40 +0200

    ~

---
+++ Stein Tünde/rekurzio/szamtani_sorozat.py
@@ -0,0 +1,18 @@
+def a(n):
+    return 3 + 2*n
+
+
+print(a(3))
+print(a(10))
+
+def b(n):
+    if n == 1:
+        eredmeny = 5
+        return eredmeny
+    else:
+        eredmeny = b(n-1)+2
+        return eredmeny
+
+
+print(b(3))
+print(b(10))
