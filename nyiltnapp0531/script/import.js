6b6e74d9fc4ee4572f45fe9c3cc3bb1ae4e4d7a0
horthyy <marton.kadarkuti@gmail.com>
2024-05-31 11:44:43 +0200

    r

--- nyiltnapp0531/script/import.js
+++ nyiltnapp0531/script/import.js
@@ -1,8 +1,8 @@
 function getClassString(group) {
     if (typeof group == 'string') {
-        return '<b>osztály: </b>' + group;
+        return group;
     } else {
-        let out = '<b>osztályok: </b>';
+        let out = '';
         for (i=0;i<group.length;i++) {
             out += group[i] + ', '
         }
@@ -23,23 +23,24 @@ function loadCards() {
             let temp = data[i];
             let currentId = temp.id.toString();
 
-            append = '<div class="card" data-period="' + temp.period + '" '; //data-period attribute a kereséshez kell
+            append = '<span class="card" data-period="' + temp.period + '" '; //data-period attribute a kereséshez kell
             append += 'id="card-' + currentId + '">'; // kell card id
 
             // ez a három lesz mutatva
-            append += '<p class="p-time"><b>időpont:</b> ' + temp.starttime + ' - ' + temp.endtime + '</p>';
-            append += '<p class="p-subject"><b>tantárgy:</b> ' + temp.subject + '</p>';
-            append += '<p class="p-room"><b>terem:</b> ' + temp.room + '</p>';
+            append += '<span class="p-time hidden">' + temp.starttime + ' - ' + temp.endtime + '</span>';
+            append += '<span class="p-subject">' + temp.subject + '</span>';
+            append += '<span class="p-room">' + temp.room + '</span>';
 
             //append += '<p><b>id:</b> ' + currentId + '</p>';  // id sneak peek
 
             //innentől elrejtve
-            append += '<p class="p-period hidden"><b>óraszám:</b> ' + temp.period + '.</p>';
-            append += '<p class="p-teacher hidden"><b>tanár:</b> ' + temp.teacher + '</p>';
-            append += '<p class="p-class hidden">' + getClassString(temp.class) + '</p>';
+            append += '<span class="p-period hidden">' + temp.period + '.</span>';
+            append += '<span class="p-teacher">' + temp.teacher + '</span>';
+            append += '<span class="p-class">' + getClassString(temp.class) + '</span>';
+            append += '<span class="p-class">' + temp.level + '</span>';
 
-            append += '></label >';
-            append += '</div>';
+            
+            append += '</span>';
             cards.innerHTML += append;
         }
 
