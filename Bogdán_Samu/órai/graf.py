43edd1841670736d2a8b64e08f68c80e5934090d
B-Samu <dacinator69@gmail.com>
2024-10-21 11:24:39 +0200

    orai

--- Bogdán_Samu/órai/graf.py
+++ Bogdán_Samu/órai/graf.py
@@ -1,18 +1,23 @@
-import random
-csucsok = list(range(10))
+forras = open('Bogdán_Samu\\órai\\labirintus.txt', 'r')
+csucsok = []
 elek = []
-for i in csucsok:
-    elek.append([])
-for i in range(15):
-    elek[random.randint(0, 9)].append(random.randint(0, 9))
-
+for i in forras:
+    el = []
+    el.append(int(i.strip().split(' -- ')[0]))
+    el.append(int(i.strip().split(' -- ')[1]))
+    csucsok.append(int(el[0]))
+    csucsok.append(int(el[1]))
+    elek.append(el)
+forras.close()
+csucsok = list(set(csucsok))
+csucsok.sort()
 print(f'{csucsok = }')
 print(f'{elek = }')
 
 s = [0]
 v = [False] * len(csucsok)
 while len(s):
-    q = s.pop()
+    q = s.pop(s[0])
     if not v[q]:
         v[q] = True
         for i in elek[q]:
