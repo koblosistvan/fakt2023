9b2df86283083b29f427e076c295387ba1b88731
B-Samu <dacinator69@gmail.com>
2024-10-24 11:44:02 +0200

    orai

--- Bogdán_Samu/órai/graf.py
+++ Bogdán_Samu/órai/graf.py
@@ -1,11 +1,9 @@
-forras = open('Bogdán_Samu\\órai\\labirintus.txt', 'r')
+forras = open('Bogdán_Samu\\órai\\labirintus.txt', 'r', encoding='utf-8')
 csucsok = list(range(100))
 elek = [[] for _ in range(100)]
 for i in forras:
-    el = []
-    el.append(int(i.strip().split(' -- ')[0]))
-    el.append(int(i.strip().split(' -- ')[1]))
-    elek[el[0]].append(el[1])
+    el = i.strip().split(' -- ')
+    elek[int(el[0])].append(int(el[1]))
 forras.close()
 print(f'{csucsok = }')
 print(f'{elek = }')
@@ -13,11 +11,13 @@ print(f'{elek = }')
 start = 10
 sor = [start]
 megvolt = [False] * 100
+szulo = [None] * 100
 while len(sor) > 0:
     start = sor[0]
-    print(start)
     for csucs in elek[start]:
         if not megvolt[csucs]:
             megvolt[csucs] = True
+            szulo[csucs] = start
             sor.append(csucs)
+    print(f'szülő: {szulo[start]}, csúcs: {start}')
     sor.remove(start)
