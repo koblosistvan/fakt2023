6d9c7e6a02bb392ee44ef1514e4806bcf21b49a7
koblosmandula <koblosmandula@gmail.com>
2024-11-24 17:55:21 +0100

    .

--- Köblös Mandula/nkp/erettsegi/minitoto.py
+++ Köblös Mandula/nkp/erettsegi/minitoto.py
@@ -39,8 +39,27 @@ for i in range(7):
 print('Gergelyiugornyai totó, 53. hét, telitalálatos szelvény:')
 for meccs in meccsek:
     print(f"{meccs[0]} - {meccs[2]} {meccs[1]}:{meccs[3]} {x12(meccs[1], meccs[3])}")
+print(f'{merkozesek} {eredmenyek}')
 
+kiiras = open('szelveny.txt', mode='w', encoding='utf-8')
+for meccs in meccsek:
+    kiiras.write(f"{meccs[0]} - {meccs[2]} {meccs[1]}:{meccs[3]} {x12(meccs[1], meccs[3])}")
+kiiras.write(f'{merkozesek} {eredmenyek}')
 
+golkulonbseg = []
+for meccs in meccsek:
+    golkulonbseg.append(abs(meccs[1]-meccs[3]))
+print(f'Legnagyobb gólkülönbségű mérkőzések: {max(golkulonbseg)}')
 
+dontetlen = 0
+for meccs in meccsek:
+    if x12(meccs[1], meccs[3]) == 'X':
+        dontetlen += 1
+if x12(eredmenyek[0], eredmenyek[2]):
+    dontetlen += 1
+print(f'Döntetlen meccsek száma: {dontetlen}')
 
+for meccs in meccsek:
+    if max(golkulonbseg) == abs(meccs[1]-meccs[3]):
+        print(meccs[0], meccs[2])
 print('Vége')
