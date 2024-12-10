6c3baddc7a0cf277b1551cc6f807cbfcef927185
horthyy <marton.kadarkuti@gmail.com>
2024-12-10 12:39:21 +0100

    12.10. orai

--- Kadarkuti_Marton/_ERETTSEGI_GYAKR/23okt25/sql_feladat/8#win.sql
+++ Kadarkuti_Marton/_ERETTSEGI_GYAKR/23okt25/sql_feladat/8#win.sql
@@ -1,4 +1,5 @@
-SELECT nevado, count(*) as be_nem_kuldott_szama FROM feladat inner join feladatsor on feladat.feladatsorid = feladatsor.id
+SELECT nevado, count(*) as be_nem_kuldott_szama FROM feladat inner join feladatsor 
+on feladat.feladatsorid = feladatsor.id
 WHERE feladat.id NOT IN (
 	SELECT feladatid FROM megoldas inner join csapat on megoldas.csapatid = csapat.id
     WHERE csapat.nev LIKE "%#win%"
