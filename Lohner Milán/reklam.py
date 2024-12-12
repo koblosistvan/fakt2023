ba956f334508bd5eee8cc11e2cddf061be338082
uranusz26 <milan.lohner10@gmail.com>
2024-12-12 12:03:54 +0100

    1

--- Lohner Milán/reklam.py
+++ Lohner Milán/reklam.py
@@ -1,4 +1,4 @@
-forras =open('rendel.txt', mode='r', encoding='utf-8')
+forras = open('rendel.txt', mode='r', encoding='utf-8')
 
 
 nap=[]
@@ -10,7 +10,7 @@ for sor in forras:
     adat=sor.strip().split(' ')
     nap.append(adat[0])
     varos.append(adat[1])
-    darab.append(adat[2])
+    darab.append(int(adat[2]))
 
 forras.close()
 
@@ -20,7 +20,17 @@ print(f'{len(darab)} darab rendelést adtak le')
 
 #3.f
 
-beker=int(input('Add meg a nap számát: '))
+beker=(input('Add meg a nap számát: '))
 
+osszead=0
 for i in range(len(nap)):
-    elso=nap[i]==1
+    if nap[i] == beker:
+         osszead += darab[i]
+print(f'{osszead} darab rendelést adtak le az adott napon')
+
+
+legtobb=max(darab)
+
+print(f'a legtobb rendeles {legtobb} volt és  napon adták le')
+
+
