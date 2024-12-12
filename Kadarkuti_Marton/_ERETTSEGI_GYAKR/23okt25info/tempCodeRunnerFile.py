6f16e9d3eb5c843438193a51eb2f545412b703f6
horthyy <marton.kadarkuti@gmail.com>
2024-12-12 22:44:33 +0100

    23okt25info hazi 01

---
+++ Kadarkuti_Marton/_ERETTSEGI_GYAKR/23okt25info/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+     if osveny[ poziciok[jelen_jatekos] ] == 'E':
+                poziciok[jelen_jatekos] += jelen_dobas
+            elif osveny[ poziciok[jelen_jatekos] ] == 'V':
+                poziciok[jelen_jatekos] -+ jelen_dobas
