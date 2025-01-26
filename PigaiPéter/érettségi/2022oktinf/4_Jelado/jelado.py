63509c712cce077c640a49fbf695899b758a71d4
Pigusz <pigaipeter@gmail.com>
2025-01-26 20:26:04 +0100

    .

--- PigaiPéter/érettségi/2022oktinf/4_Jelado/jelado.py
+++ PigaiPéter/érettségi/2022oktinf/4_Jelado/jelado.py
@@ -41,3 +41,25 @@ for i in range(len(kord)-1):
 print(f"6. feladat \nElmozdulás: {round(elmozdulas,3)} egység ")
 
 #7
+kimaradt = open("PigaiPéter\\érettségi\\2022oktinf\\4_Jelado\\kimaradt.txt", mode="w", encoding="utf-8")
+
+for i in range(len(ido)-1):
+    idohiba = 0
+    kordhiba = 0
+    if eltelt(ido[i+1], ido[i]) > 5*60:
+        elteres = eltelt(ido[i+1], ido[i])
+        while elteres > 5*60:
+            idohiba += 1
+            elteres -= 5*60
+    if pow((kord[i][0]-kord[i+1][0])**2+(kord[i][1]-kord[i+1][1])**2, 0.5) > 10:
+        tav = pow((kord[i][0]-kord[i+1][0])**2+(kord[i][1]-kord[i+1][1])**2, 0.5)
+        while tav > 10:
+            kordhiba += 1
+            tav -= 10
+    if idohiba != 0 or kordhiba != 0:
+        if idohiba > kordhiba:
+            kimaradt.write(f"{ido[i + 1][0]} {ido[i + 1][1]} {ido[i + 1][2]} időeltérés {idohiba}\n")
+        elif idohiba < kordhiba:
+            kimaradt.write(f"{ido[i + 1][0]} {ido[i + 1][1]} {ido[i + 1][2]} koordináta-eltérés {kordhiba}\n")
+        else:
+            kimaradt.write(f"{ido[i + 1][0]} {ido[i + 1][1]} {ido[i + 1][2]} időeltérés {idohiba}\n")
