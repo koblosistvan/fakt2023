856eb58120024e53b16327ee2110ea77a2a4df0e
horthyy <marton.kadarkuti@gmail.com>
2025-01-28 12:40:52 +0100

    0128 orai

--- Kadarkuti_Marton/_ERETTSEGI_GYAKR/info/info_22okt/sql_feladat/5szlovak.sql
+++ Kadarkuti_Marton/_ERETTSEGI_GYAKR/info/info_22okt/sql_feladat/5szlovak.sql
@@ -1,3 +1,11 @@
 select palya.ut, kesz
 from palya inner join telepules on palya.ut = telepules.ut
 where lcase(hatar) like "%szlovákia%"
+
+/*
+A pont jár akkor is, ha a kész autópályák közül a 0 km hosszúakat a megjelenítésből kizárta.
+Például:
+SELECT palya.ut, kesz
+FROM palya, vege, telepules
+WHERE palya.ut=vege.ut AND telepid=telepules.id AND hatar='Szlovákia';
+*/
