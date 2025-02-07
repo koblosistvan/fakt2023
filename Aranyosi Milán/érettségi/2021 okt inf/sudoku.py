38008b36ef8d1ee543d37dad367565ff0ae87017
aranyosimilan <cementes8@gmail.com>
2025-02-07 12:27:30 +0100

    0207

---
+++ Aranyosi Milán/érettségi/2021 okt inf/sudoku.py
@@ -0,0 +1,25 @@
+print('1. feladat')
+
+
+
+
+'''
+
+Adja meg a bemeneti fájl nevét! konnyu.txt
+Adja meg egy sor számát! 1
+Adja meg egy oszlop számát! 1
+3. feladat
+Az adott helyen szereplő szám: 5
+A hely a(z) 1 résztáblázathoz tartozik.
+4. feladat
+Az üres helyek aránya: 17.3%
+5. feladat
+A kiválasztott sor: 2 oszlop: 4 a szám: 9
+A helyet már kitöltötték.
+A kiválasztott sor: 3 oszlop: 6 a szám: 7
+A lépés megtehető.
+A kiválasztott sor: 6 oszlop: 6 a szám: 3
+A résztáblázatban már szerepel a szám.
+A kiválasztott sor: 7 oszlop: 9 a szám: 8
+Az adott oszlopban már szerepel a szám. 
+'''
