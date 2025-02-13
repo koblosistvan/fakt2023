4d9c2fd81e2fa2857797ac83141d0c8fed68dee1
callisto0a <koblosmandula@gmail.com>
2025-02-13 12:38:54 +0100

    .

--- Köblös Mandula/emelt érettségi/2021máj/godor.py
+++ Köblös Mandula/emelt érettségi/2021máj/godor.py
@@ -10,11 +10,11 @@ forras.close()
 print('1. feladat')
 print(f'A féjl adatainak száma: {len(melyseg)}')
 
-print('2. feladat')
+print('\n2. feladat')
 tavolsag = int(input('Adjon meg egy távolságértéket! '))
 print(f'Ezen a helyen a felszín {melyseg[tavolsag]} méter mélyen van.')
 
-print('3. feladat')
+print('\n3. feladat')
 erintetlen = 0
 for i in range(len(melyseg)):
     if melyseg[i] == 0:
@@ -23,5 +23,27 @@ print(f'Az érintetlen terület aránya {erintetlen/len(melyseg):0.2%}')
 
 kiiras = open('godrok.txt', mode='w', encoding='utf-8')
 
+godrok = []
 for i in range(len(melyseg)):
     if melyseg[i] != 0:
+        godrok.append(melyseg[i])
+    elif melyseg[i] == 0 and melyseg[i-1] != 0:
+        godrok.append(melyseg[i])
+
+
+for i in range(len(godrok)):
+    if godrok[i] != 0:
+        print(godrok[i] ,end=" " ,file=kiiras)
+    else:
+        print('', file=kiiras)
+
+print('\n5.feladat')
+godrok_szama = 0
+for i in range(len(godrok)):
+    if godrok[i] == 0:
+        godrok_szama += 1
+print(f'A gördök száma: {godrok_szama}')
+
+print('\n6.feladat')
+if melyseg[tavolsag] == 0:
+    print('Az adott helyen nincs gödör.')
