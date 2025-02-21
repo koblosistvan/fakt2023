4c2bbb119659c3134a105c552194df4f6ece7156
aranyosimilan <cementes8@gmail.com>
2025-02-21 13:29:35 +0100

    .

--- Aranyosi Milán/érettségi/2021 maj inf kulfoldiul/banyato.py
+++ Aranyosi Milán/érettségi/2021 maj inf kulfoldiul/banyato.py
@@ -9,7 +9,6 @@ for sor in forras:
     for i in range(len(adat)):
         adat[i] = int(adat[i])
     melysegek.append(adat)
-print(melysegek)
 
 print('2. feladat')
 
@@ -18,23 +17,60 @@ adott_oszlop = int(input('A mérés oszlopának azonosítója=')) - 1
 print(f'A mért mélység az adott helyen {melysegek[adott_sor][adott_oszlop]} dm')
 
 print('\n3. feladat')
-seged_melysegek = 0
+osszes_melysegek = 0
+felszin = 0
 for i in range(len(melysegek)):
-    pass
+    for j in range(len(melysegek[i])):
+        if melysegek[i][j] != 0:
+            felszin += 1
+            osszes_melysegek += melysegek[i][j]
+
+print(f'A tó felszíne: {felszin} m2, átlagos mélysége: {osszes_melysegek/felszin/10:0.2f} m')
+
+print(f'\n4. feladat')
+
+legnagyobb_melysegek = melysegek[0][0]
+
+for i in range(len(melysegek)):
+    for j in range(len(melysegek[i])):
+        if melysegek[i][j] > legnagyobb_melysegek:
+            legnagyobb_melysegek = melysegek[i][j]
+
+print(f'A tó legnagyobb mélysége: {legnagyobb_melysegek} dm')
+print('A legmélyebb helyek sor-oszlop koordinátái:')
+
+for i in range(len(melysegek)):
+    for j in range(len(melysegek[i])):
+        if melysegek[i][j] == legnagyobb_melysegek:
+            print(f'({i+1};{j+1})', end='\t')
 
 
+print('\n\n5. feladat')
 
-print(f'A tó felszíne: {} m2, átlagos mélysége: {} m')
+kerulet = 0
 
-'''
+for i in range(len(melysegek)):
+    for j in range(len(melysegek[i])):
+        if melysegek[i][j] != 0:
+            if melysegek[i-1][j] == 0:
+                kerulet += 1
+            if melysegek[i+1][j] == 0:
+                kerulet += 1
+            if melysegek[i][j-1] == 0:
+                kerulet += 1
+            if melysegek[i][j+1] == 0:
+                kerulet += 1
+
+print(f'A tó partvonala {kerulet} m hosszú')
 
 
-4. feladat
-A tó legnagyobb mélysége: 98 dm
-A legmélyebb helyek sor-oszlop koordinátái:
-(14; 20) (26; 11) (32; 16)
-5. feladat
-A tó partvonala 270 m hosszú
-6. feladat
-A vizsgált szelvény oszlopának azonosítója=6 
-'''
+print('\n6.feladat')
+vizsgalt_oszlop = int(input('A vizsgált szelvény oszlopának azonosítója=')) - 1
+
+kimenet = open('diagram.txt', mode='w', encoding='utf-8')
+
+for i in range(len(melysegek[i])):
+    print(f'{i+1:02}{melysegek[i][vizsgalt_oszlop] * "*"}', file=kimenet)
+
+
+kimenet.close()
